a6a1f15f1dbe00ba8960d6f4db130d6fa8347424
liellison <liellisonjoseph@gmail.com>
2016-06-27 10:37:54 -0300

    Inicio da class/tela de 90 horas

---
+++ app/src/main/java/com/liellison/calcuern/Horas90Activity.java
@@ -0,0 +1,7 @@
+package com.liellison.calcuern;
+
+/**
+ * Created by liellison on 27/06/16.
+ */
+public class Horas90Activity {
+}
--- app/src/main/java/com/liellison/calcuern/MainActivity.java
+++ app/src/main/java/com/liellison/calcuern/MainActivity.java
@@ -2,12 +2,16 @@ package com.liellison.calcuern;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+	Button button, botaotestid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    button = (Button) findViewById (R.id.button);
     }
 }
