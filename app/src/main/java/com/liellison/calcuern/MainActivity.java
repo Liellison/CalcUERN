10c301e89d1c546093f3da7cf730bdfe5d3ff6e9
liellison <liellisonjoseph@gmail.com>
2016-08-01 15:56:00 -0300

    Versao 0.0.1 Atualizaçao da tela sobre materias com 90h

--- app/src/main/java/com/liellison/calcuern/MainActivity.java
+++ app/src/main/java/com/liellison/calcuern/MainActivity.java
@@ -1,17 +1,21 @@
 package com.liellison.calcuern;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-	Button button, botaotestid;
+public class MainActivity extends Activity {
+	Button bt90horas, bt30horas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    button = (Button) findViewById (R.id.button);
+        bt90horas = (Button) findViewById (R.id.bt90horas);
+        bt30horas = (Button) findViewById(R.id.bt30horas);
+
+
     }
 }
