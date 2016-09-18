24567a0e07ee95b78b8a28cc38b27ab1eb2b969b
liellison <liellisonjoseph@gmail.com>
2016-09-18 14:05:47 -0300

    Versao Alpha 0.0.2.3 Inicio de testes para a possibilidade do aplicativo ter uma versao paga e outra gratis

---
+++ app/src/free/java/com/liellison/calcuern/free/MainActivity.java
@@ -0,0 +1,15 @@
+package com.liellison.calcuern.free;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.liellison.calcuern.R;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
